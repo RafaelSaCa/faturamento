7906ebf465d54cf45b5ebe86fae15ff636e841d1
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-04 23:11:12 -0300

    feat: Cliente Request

---
+++ src/main/java/com/rafaelsaca/faturamento/dto/ClienteRequest.java
@@ -0,0 +1,10 @@
+package com.rafaelsaca.faturamento.dto;
+
+public class ClienteRequest {
+    
+    public String nome;
+    public String cpfCnpj;
+    public String email;
+    public String telefone;
+    public String cep;
+}
