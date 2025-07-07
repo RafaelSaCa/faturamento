3980fd60437b0126e947446c98bddb5a31e5ba38
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-06 22:17:23 -0300

    feat: Cliente entidade

--- src/main/java/com/rafaelsaca/faturamento/model/Cliente.java
+++ src/main/java/com/rafaelsaca/faturamento/model/Cliente.java
@@ -1,18 +1,17 @@
 package com.rafaelsaca.faturamento.model;
 
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Embeddable
+@Entity
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
     private String cpfCnpj;
     private String email;
