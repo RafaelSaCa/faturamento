adb36ff4482876403129c5d237e879340e6e79dd
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-06 22:18:59 -0300

    feat: Cliente Request  - validações

--- src/main/java/com/rafaelsaca/faturamento/dto/ClienteRequest.java
+++ src/main/java/com/rafaelsaca/faturamento/dto/ClienteRequest.java
@@ -1,10 +1,78 @@
 package com.rafaelsaca.faturamento.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class ClienteRequest {
-    
-    public String nome;
-    public String cpfCnpj;
-    public String email;
-    public String telefone;
-    public String cep;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "CPF/CNPJ é obrigatório")
+    private String cpfCnpj;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Telefone é obrigatório")
+    private String telefone;
+
+    @NotBlank(message = "CEP é obrigatório")
+    private String cep;
+
+    @NotBlank(message = "Número é obrigatório")
+    private String numero;
+
+    public ClienteRequest() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
 }
