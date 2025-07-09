f0ac5bac42f75fd0a496737718694f808de7ed76
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-09 18:00:16 -0300

    feat: Cobrança repository

--- src/main/java/com/rafaelsaca/faturamento/repository/CobrancaRepository.java
+++ src/main/java/com/rafaelsaca/faturamento/repository/CobrancaRepository.java
@@ -1,6 +1,6 @@
 package com.rafaelsaca.faturamento.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +8,5 @@ import com.rafaelsaca.faturamento.model.Cobranca;
 
 public interface CobrancaRepository extends JpaRepository<Cobranca,Long>{
 
-   Optional<Cobranca> findByClienteId (Long clienteId);
+   List<Cobranca> findByClienteId (Long clienteId);
 }
