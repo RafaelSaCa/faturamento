d7bd0e89a270a29b80df5e0885e4aa6598929a34
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-08 21:56:58 -0300

    feat: Cobrança Response

---
+++ src/main/java/com/rafaelsaca/faturamento/dto/CobrancaResponse.java
@@ -0,0 +1,70 @@
+package com.rafaelsaca.faturamento.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.rafaelsaca.faturamento.model.Cliente;
+import com.rafaelsaca.faturamento.model.Cobranca.Status;
+import com.rafaelsaca.faturamento.model.Cobranca.Tipo;
+
+public class CobrancaResponse {
+
+    private BigDecimal valor;
+    private String descricao;
+    private LocalDate dataVencimento;
+    private Tipo tipo;
+    private Status status;
+    private Cliente cliente;
+
+    public CobrancaResponse() {
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+}
