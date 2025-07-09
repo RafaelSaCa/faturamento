2d7570617c0b503546c34271d5fd0e35166eb66d
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-09 17:58:17 -0300

    feat: Exception - recurso não encontrado, para exibir mensagem mais limpa

--- src/main/java/com/rafaelsaca/faturamento/controller/GlobalExceptionHandler.java
+++ src/main/java/com/rafaelsaca/faturamento/controller/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.rafaelsaca.faturamento.controller;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.rafaelsaca.faturamento.exception.RecursoNaoEncontradoException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,6 +24,18 @@ public class GlobalExceptionHandler {
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+
+     @ExceptionHandler(RecursoNaoEncontradoException.class)
+    public ResponseEntity<?> handleClienteNaoEncontrado(RecursoNaoEncontradoException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", "Recurso não encontrado");
+        body.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 
 }
