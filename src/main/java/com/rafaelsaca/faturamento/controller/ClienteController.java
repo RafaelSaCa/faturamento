683906ae4907de7e77254949dcb7e085ac748e23
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-08 21:55:18 -0300

    feat: Cliente controller - patchMapping atualização parcial dos dados do cliente

--- src/main/java/com/rafaelsaca/faturamento/controller/ClienteController.java
+++ src/main/java/com/rafaelsaca/faturamento/controller/ClienteController.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;  
 
 import com.rafaelsaca.faturamento.dto.ClienteRequest;
 import com.rafaelsaca.faturamento.dto.ClienteResponse;
@@ -43,8 +43,8 @@ public class ClienteController {
         return ResponseEntity.ok().body(lista);
     }
 
-    @PutMapping("/{clienteId}")
-    public ResponseEntity<ClienteResponse> update(@PathVariable Long clienteId, @Valid @RequestBody ClienteRequest request) {
+    @PatchMapping("/{clienteId}")
+    public ResponseEntity<ClienteResponse> update(@PathVariable Long clienteId,@RequestBody ClienteRequest request) {
         ClienteResponse cliente = service.update(clienteId, request);
 
         return ResponseEntity.ok().body(cliente);
