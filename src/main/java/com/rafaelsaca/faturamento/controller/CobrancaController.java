3aab66c3d720cc15b08e344dd00408d37734e542
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-08 21:57:47 -0300

    feat: Cobrança controller

---
+++ src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
@@ -0,0 +1,40 @@
+package com.rafaelsaca.faturamento.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaelsaca.faturamento.dto.CobrancaRequest;
+import com.rafaelsaca.faturamento.dto.CobrancaResponse;
+import com.rafaelsaca.faturamento.service.CobrancaService;
+
+@RestController
+@RequestMapping("/cobranca")
+public class CobrancaController {
+
+    private final CobrancaService service;
+
+    public CobrancaController(CobrancaService service) {
+        this.service = service;
+    }
+
+    @PostMapping()
+    public ResponseEntity<CobrancaResponse> gerarCobranca(@RequestBody CobrancaRequest request) {
+        CobrancaResponse cobranca = service.create(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cobranca);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<CobrancaResponse>> getAll() {
+        List<CobrancaResponse> lista = service.findAll();
+
+        return ResponseEntity.ok().body(lista);
+    }
+}
