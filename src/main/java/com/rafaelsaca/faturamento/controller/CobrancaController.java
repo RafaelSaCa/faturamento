7c6e4e24320c631e267823210656619b6ce38eb0
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-09 18:01:09 -0300

    feat: Cobrança controller - busca de cobrança por id do cliente

--- src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
+++ src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,12 @@ public class CobrancaController {
         List<CobrancaResponse> lista = service.findAll();
 
         return ResponseEntity.ok().body(lista);
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity <List<CobrancaResponse>> findByClienteId(@PathVariable Long clienteId) {
+        List<CobrancaResponse> cobrancas = service.findByClienteId(clienteId);
+
+        return ResponseEntity.ok().body(cobrancas);
     }
 }
