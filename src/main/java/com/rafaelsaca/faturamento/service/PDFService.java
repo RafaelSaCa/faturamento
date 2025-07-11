8c55b9476a3a36e398b4dbc190096ae6e9d96878
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-11 13:15:45 -0300

    feat: PDFService

---
+++ src/main/java/com/rafaelsaca/faturamento/service/PDFService.java
@@ -0,0 +1,82 @@
+package com.rafaelsaca.faturamento.service;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.time.format.DateTimeFormatter;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.stereotype.Service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+import com.rafaelsaca.faturamento.dto.CobrancaResponse;
+
+@Service
+public class PDFService {
+
+  
+    public byte[] gerarCobrancaPDF(CobrancaResponse cobranca) throws Exception {
+
+        ByteArrayOutputStream arrayByte = new ByteArrayOutputStream();
+        Document doc = new Document();
+        PdfWriter.getInstance(doc, arrayByte);
+        doc.open();
+
+        Font tituloFont = new Font(Font.HELVETICA, 16, Font.BOLD);
+        Font normalFont = new Font(Font.HELVETICA, 12);
+
+        doc.add(new Paragraph("        COBRANÇA     ", tituloFont));
+        doc.add(new Paragraph(""));
+
+
+        doc.add(new Paragraph("Sacado: " + cobranca.getCliente().getNome(), normalFont));
+        doc.add(new Paragraph("Valor: R$" + cobranca.getValor(), normalFont));
+        doc.add(new Paragraph(
+                "Vencimento: " + cobranca.getDataVencimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                normalFont));
+        doc.add(new Paragraph("Tipo: " + cobranca.getTipo(), normalFont));
+        doc.add(new Paragraph("Descrição: " + cobranca.getDescricao(), normalFont));
+        doc.add(new Paragraph(" "));
+
+
+        if (cobranca.getTipo().name().equals("BOLETO")) {
+            doc.add(new Paragraph("Linha digitável: 00190.00009 01234.567899 12345.600000 1 12340000015000",
+                    normalFont));
+            doc.add(new Paragraph("Código para simular o pagamento do boleto.", normalFont));
+
+        } else if (cobranca.getTipo().name().equals("PIX")) {
+            String payLoadPix = "00020126580014BR.GOV.BCB.PIX0136email@pix.com";
+
+            doc.add(new Paragraph("Código PIX:", normalFont));
+            doc.add(new Paragraph(payLoadPix, new Font(Font.COURIER, 10)));
+            doc.add(new Paragraph(" "));
+
+            Image img = gerarImagemQRCode(payLoadPix);
+            img.scalePercent(100);
+            doc.add(img);
+
+        }
+
+        doc.close();
+        return arrayByte.toByteArray();
+    }
+
+    public Image gerarImagemQRCode(String texto) throws Exception {
+        BitMatrix matrix = new MultiFormatWriter().encode(texto, BarcodeFormat.QR_CODE, 200, 200);
+        BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(matrix);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", baos);
+        baos.flush();
+
+        return Image.getInstance(baos.toByteArray());
+    }
+}
