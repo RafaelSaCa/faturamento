ff7cf773ec15bea85927b9a47278f2096a456dab
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-11 13:17:30 -0300

    feat: Cobranca Controller - gera cobrança em arquivo pdf

--- src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
+++ src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
@@ -2,7 +2,9 @@ package com.rafaelsaca.faturamento.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,22 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rafaelsaca.faturamento.dto.CobrancaRequest;
 import com.rafaelsaca.faturamento.dto.CobrancaResponse;
 import com.rafaelsaca.faturamento.service.CobrancaService;
+import com.rafaelsaca.faturamento.service.PDFService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/cobranca")
 public class CobrancaController {
 
     private final CobrancaService service;
+    private final PDFService pdfService;
 
-    public CobrancaController(CobrancaService service) {
+    public CobrancaController(CobrancaService service, PDFService pdfService) {
         this.service = service;
+        this.pdfService = pdfService;
     }
 
     @PostMapping()
-    public ResponseEntity<CobrancaResponse> gerarCobranca(@RequestBody CobrancaRequest request) {
-        CobrancaResponse cobranca = service.create(request);
+    public ResponseEntity<byte[]> gerarCobranca(@RequestBody @Valid CobrancaRequest request) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(cobranca);
+        CobrancaResponse cobranca = service.create(request);
+        byte[] pdf;
+
+        try {
+
+            pdf = pdfService.gerarCobrancaPDF(cobranca);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_PDF)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=cobranca_" + cobranca.getId() + ".pdf")
+                .body(pdf);
     }
 
     @GetMapping()
@@ -40,7 +61,7 @@ public class CobrancaController {
     }
 
     @GetMapping("/{clienteId}")
-    public ResponseEntity <List<CobrancaResponse>> findByClienteId(@PathVariable Long clienteId) {
+    public ResponseEntity<List<CobrancaResponse>> findByClienteId(@PathVariable Long clienteId) {
         List<CobrancaResponse> cobrancas = service.findByClienteId(clienteId);
 
         return ResponseEntity.ok().body(cobrancas);
