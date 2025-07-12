04a6b4902db2af8601cbe82ea2775d3d99f1e752
RafaelSaCa <raffaellsaca@gmail.com>
2025-07-11 22:35:11 -0300

    feat: Cobranca controller

--- src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
+++ src/main/java/com/rafaelsaca/faturamento/controller/CobrancaController.java
@@ -2,9 +2,7 @@ package com.rafaelsaca.faturamento.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rafaelsaca.faturamento.dto.CobrancaRequest;
 import com.rafaelsaca.faturamento.dto.CobrancaResponse;
 import com.rafaelsaca.faturamento.service.CobrancaService;
-import com.rafaelsaca.faturamento.service.PDFService;
 
 import jakarta.validation.Valid;
 
@@ -25,32 +22,20 @@ import jakarta.validation.Valid;
 public class CobrancaController {
 
     private final CobrancaService service;
-    private final PDFService pdfService;
 
-    public CobrancaController(CobrancaService service, PDFService pdfService) {
+
+    public CobrancaController(CobrancaService service) {
         this.service = service;
-        this.pdfService = pdfService;
+        
     }
 
     @PostMapping()
-    public ResponseEntity<byte[]> gerarCobranca(@RequestBody @Valid CobrancaRequest request) {
+    public ResponseEntity<CobrancaResponse> gerarCobranca(@RequestBody @Valid CobrancaRequest request) {
 
         CobrancaResponse cobranca = service.create(request);
-        byte[] pdf;
 
-        try {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cobranca);
 
-            pdf = pdfService.gerarCobrancaPDF(cobranca);
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .contentType(MediaType.APPLICATION_PDF)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=cobranca_" + cobranca.getId() + ".pdf")
-                .body(pdf);
     }
 
     @GetMapping()
